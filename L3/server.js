cdaabb8df0614e2b755d3da888be7c0f368efcc3
Vsevolod Alexeenko <vsevolod.alexeenko@nrg-soft.com>
2018-03-11 21:49:28 +0300

    Server

--- L3/server.js
+++ L3/server.js
@@ -1,1 +1,17 @@
-console.log('Hello World');
+const log = require('./Log');
+const http = require('http');
+const hostName = '127.0.0.1';
+const port = 3000;
+
+const eventLog = log.eventLog;
+
+const server = http.createServer((req, res) => {
+res.statusCode = 200;
+res.setHeader('Content-Type', 'text/plain');
+res.end(req.url);
+eventLog.emit('request', req.url);
+});
+
+server.listen(port, hostName, () => {
+console.log('Server running');
+});
